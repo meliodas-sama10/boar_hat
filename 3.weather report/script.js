d9be3434ba6da315a054dc2b73ea1931dc543743
Rishav <135334779+meliodas-sama10@users.noreply.github.com>
2024-05-29 01:09:32 +0530

    js

---
+++ 3.weather report/script.js
@@ -0,0 +1,4 @@
+const cityInput=document.querySelector(".city-input");
+const searchButton=document.querySelector(".search-btn");
+const currentWeatherDiv=document.querySelector(".current-weather");
+const weatherCardDiv=document.querySelector(".weather-card");
